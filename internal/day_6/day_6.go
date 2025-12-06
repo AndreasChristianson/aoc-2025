62cd012701dcbed8e1466889cc60068bb1225f0a
Andreas Christianson <achristianson@pessimistic-it.com>
2025-12-05 22:59:29 -0600

    setup

---
+++ internal/day_6/day_6.go
@@ -0,0 +1,17 @@
+package day_6
+
+import (
+	"strconv"
+)
+
+func part1(lines []string) string {
+	var count int64
+
+	return strconv.FormatInt(count, 10)
+}
+
+func part2(lines []string) string {
+	var count int64
+
+	return strconv.FormatInt(count, 10)
+}
